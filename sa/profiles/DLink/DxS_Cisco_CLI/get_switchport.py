2978b710d80c04935f6a0ecd1048c2dc7ff07c05
santor72 <santor@ccs.ru>
2021-06-14 22:04:45 +0300

    sdasdasd

--- sa/profiles/DLink/DxS_Cisco_CLI/get_switchport.py
+++ sa/profiles/DLink/DxS_Cisco_CLI/get_switchport.py
@@ -1,67 +1,82 @@
 # ---------------------------------------------------------------------
-# DLink.DxS_Cisco_CLI.get_switchport
+# Generic.get_switchport
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2020 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # Python modules
-import re
+from itertools import compress
 
-# NOC modules
+# NOC Modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetswitchport import IGetSwitchport
+from noc.core.mib import mib
+from noc.core.snmp.render import render_bin
 
 
 class Script(BaseScript):
-    name = "DLink.DxS_Cisco_CLI.get_switchport"
+    name = "Generic.get_switchport"
     interface = IGetSwitchport
-    rx_cont = re.compile(r"$\s+(?=\d+)", re.MULTILINE)
-    rx_line = re.compile(
-        r"^(?P<interface>\S*\s*\d+(\/\d+)?)\s+(?P<status>\S+)\s+"
-        r"(?P<mode>ACCESS|TRUNK)\s+(?P<access_vlan>\d+)\s+"
-        r"(?P<untagged>\d+)\s+\S+\s+(?P<vlans>\S+)?\n",
-        re.MULTILINE,
-    )
 
-    def execute(self):
-        r = []
-        c = self.cli("show interfaces switchport")
-        c = self.rx_cont.sub(",", c)  # Unwind continuation lines
-
-        for match in self.rx_line.finditer(c):
-            trunk = match.group("mode") == "TRUNK"
-            pvid = None
-            if trunk:
-                pvid = int(match.group("untagged"))
-                vlans = match.group("vlans")
-                if vlans is not None and vlans != "":
-                    if vlans == "ALL":
-                        tagged = list(range(1, 4095))
-                    else:
-                        tagged = self.expand_rangelist(vlans)
-                else:
-                    tagged = []
-            else:
-                pvid = int(match.group("access_vlan"))
-                tagged = []
-            members = []
-            interface = match.group("interface")
-            if interface.startswith("AggregatePort"):
-                shortname = self.profile.convert_interface_name(interface)
-                for p in self.scripts.get_portchannel():
-                    if p["interface"] == shortname:
-                        members = p["members"]
-            r += [
-                {
-                    "interface": interface,
-                    "status": match.group("status") == "enabled",
-                    # "description"    : "Dummy";
-                    "802.1Q Enabled": trunk,
-                    "802.1ad Tunnel": False,
-                    "untagged": pvid,
-                    "tagged": tagged,
-                    "members": members,
-                }
-            ]
+    def get_iface_portid_mapping(self):
+        # Get PID -> ifindex mapping
+        r = {}
+        for oid, v in self.snmp.getnext(mib["BRIDGE-MIB::dot1dBasePortIfIndex"]):
+            r[oid.split(".")[-1]] = v
         return r
+
+    @staticmethod
+    def convert_vlan(vlans):
+        """
+
+        :param vlans: Byte string FF 00 01 80 ....
+        :return: itera
+        """
+        for line in vlans.splitlines():
+            for vlan_pack in line.split()[0]:
+                # for is_v in bin(int(vlan_pack, 16)):
+                for is_v in "{0:08b}".format(vlan_pack):
+                    yield int(is_v)
+
+    def execute_snmp(self, **kwargs):
+        result = {}
+        names = {x: y for y, x in self.scripts.get_ifindexes().items()}
+        # Get PID -> ifindex mapping
+        pid_ifindex_mappings = self.get_iface_portid_mapping()
+        iface_list = sorted(pid_ifindex_mappings, key=int)
+
+        # Getting PVID
+        for oid, pvid in self.snmp.getnext(mib["Q-BRIDGE-MIB::dot1qPvid"]):
+            if not pvid:
+                # if pvid is 0
+                continue
+            o = oid.split(".")[-1]
+            # pvid[pid_ifindex_mappings[o]] = v
+            result[o] = {
+                "interface": names[pid_ifindex_mappings[o]],
+                "status": False,
+                # "ifindex": ifindex,
+                # "port_type": port_type,
+                "untagged": pvid,
+                "tagged": [],
+                "members": [],
+            }
+        # Getting Tagged
+        decode_ports_list = {}
+        for oid, ports_list in self.snmp.getnext(
+            mib["Q-BRIDGE-MIB::dot1qVlanCurrentEgressPorts"],
+            display_hints={mib["Q-BRIDGE-MIB::dot1qVlanCurrentEgressPorts"]: render_bin},
+        ):
+            vlan_num = int(oid.split(".")[-1])
+            if ports_list not in decode_ports_list:
+                # ports_list_mask = ["{0:04b}".format(int(x, 16)) for x in ports_list]
+                decode_ports_list[ports_list] = list(
+                    compress(iface_list, [int(x) for x in self.convert_vlan(ports_list)])
+                )
+            for p in decode_ports_list[ports_list]:
+                if vlan_num == result[p]["untagged"]:
+                    # Perhaps port is switchport @todo getting port type
+                    continue
+                result[p]["tagged"] += [vlan_num]
+        return list(result.values())
--- sa/profiles/ZTE/ZXA10/profile.py
+++ sa/profiles/ZTE/ZXA10/profile.py
@@ -24,7 +24,7 @@ class Profile(BaseProfile):
     command_enter_config = "configure terminal"
     command_leave_config = "exit"
     command_save_config = "write\n"
-    pattern_prompt = r"^(?P<hostname>\S+?)(?:-\d+)?(?:\(config[^\)]*\))?#"
+    pattern_prompt = r"^(?P<hostname>\S+?)(?:-\d+)?(?:\(config[^\)]*\))?(?:\(gpon-onu-mng \S+\))?#"
     requires_netmask_conversion = True
     convert_mac = BaseProfile.convert_mac_to_cisco
     config_volatile = [r"^ntp clock-period .*?^"]
