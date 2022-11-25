8e448d2d117653f4abef8866ef95658c1508e5fa
santor72 <santor@ccs.ru>
2022-11-25 13:44:18 +0300

    new ping

--- services/nbi/api/ping.py
+++ services/nbi/api/ping.py
@@ -1,70 +1,49 @@
-# -*- coding: utf-8 -*-
-# ----------------------------------------------------------------------
-# config API
-# ----------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
-# See LICENSE for details
-# ----------------------------------------------------------------------
-
 # Python modules
-from __future__ import absolute_import
-import time
 import os
-import json
+#from typing import List, Union
 
 # Third-party modules
-import tornado.gen
+from fastapi import APIRouter, Header, HTTPException
+from pydantic import BaseModel
 
 # NOC modules
-from noc.core.service.apiaccess import authenticated
-from noc.sa.models.managedobject import ManagedObject
-from noc.services.nbi.base import NBIAPI
-# Third-party modules
-from tornado.ioloop import IOLoop
-import tornado.queues
+from noc.sa.models.objectstatus import ObjectStatus
+from from noc.services.nbi.base import NBIAPI, API_ACCESS_HEADER, FORBIDDEN_MESSAGE
 
-# NOC modules
-from noc.core.management.base import BaseCommand
-from noc.core.validators import is_ipv4
-from noc.core.ioloop.ping import Ping
-from noc.config import config
+router = APIRouter()
 
 
-class PingAPI(NBIAPI):
-    name = "ping"
+class ObjectPingRequest(BaseModel):
+    ip: str
 
-    @authenticated
-    @tornado.gen.coroutine
-    def get(self, ip):
-        code, result = yield self.executor.submit(self.handler, ip)
-        self.set_status(code)
-        self.set_header("Content-Type", "text/plain")
-        if code != 204:
-            self.write(result)
 
-    def handler(self, ip):
-        if is_ipv4(ip):
-           return 200, json.dumps(self.doping(ip));
-        else:
-           try:
-               req = json.loads(self.request.body)
-               r = []
-               for n in req:
-                   if is_ipv4(n):
-                      r.append(self.doping(ip))
-                   return 200, json.dumps(r)
-           except ValueError:
-               return 400, "Cannot decode JSON"
+class ObjectPingResponse(BaseModel):
+    result: str
+
+class ObjectPingAPI(NBIAPI):
+    api_name = "ping"
+    openapi_tags = ["ping API"]
+
+    def get_routes(self):
+        route = {
+            "path": "/api/nbi/ping",
+            "method": "GET",
+            "endpoint": self.handler,
+            "response_model": ObjectPingResponse,
+            "name": "ping",
+            "description": "Ping one Managed Objects.",
+        }
+        return [route]
+
+    async def handler(self, req: ObjectStatusRequest, access_header: str = Header(..., alias=API_ACCESS_HEADER):
+        if not self.access_granted(access_header):
+            raise HTTPException(403, FORBIDDEN_MESSAGE)
+        if not is_ipv4(req.ip):
+            raise HTTPException(400, "Bad request: no ip address")
+        return {result: self.doping(req.ip)}
 
     def doping(self, ip):
-        if is_ipv4(ip):
-           if os.system('ping -c 2 -W 1 %s > /dev/null'%ip) == 0:
-             return {ip : 'Up'};
-           else:
+        if os.system('ping -c 2 -W 1 %s > /dev/null'%ip) == 0:
+            return {ip : 'Up'};
+        else:
              return {ip : 'Down'};
-    
-    
-    @classmethod
-    def get_path(cls):
-        return r"%s" % cls.name, r"%s/(.+)" % cls.name, r"%s/(.+)/(.+)" % cls.name
-
