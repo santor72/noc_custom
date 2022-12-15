6505f359f394f0a2f9c6cd5e3908532273ab0f2c
santor72 <santor@ccs.ru>
2022-12-15 16:51:06 +0300

    ospf

---
+++ commands/ospf.py
@@ -0,0 +1,178 @@
+# coding: utf-8
+#python modules
+import re
+from pprint import pprint
+import pickle
+import copy
+
+#NOC modules
+from noc.core.mongo.connection import connect
+from noc.sa.models.action import Action
+from noc.sa.models.managedobject import ManagedObject
+from noc.inv.models.interface import Interface
+from noc.inv.models.subinterface import SubInterface
+
+def Huawei_VRP_get_ospf_process_peers(mo=None, pid=None, peers=[]):
+    try:
+        if not mo:
+            return 0
+        if not pid:
+            return 0
+        ospfne = mo.scripts.commands(commands = ['dis ospf {} peer  brief'.format(pid)])
+        if not ospfne['output']:
+            return -1
+        l = ospfne['output'][0].split('\n')
+        re_peer = re.compile(r"^.+(?P<area>\d+.\d+.\d+.\d+)\s+(?P<interface>\S+)\s+(?P<ip>\d+.\d+.\d+.\d+)\s+(?P<state>\S+)\s+")
+        for neitem in l:
+            m_ne = re_peer.match(neitem)
+            if m_ne:
+                peers.append(
+                  {
+                    'peer_id': m_ne.group('ip'),
+                    'state': m_ne.group('state'),
+                    'peer_address': '',
+                    'interface': m_ne.group('interface'),
+                    'area': m_ne.group('area')
+                  }
+                )
+        return 1
+    except:
+        return -1
+
+def Huawei_VRP_get_ospf_peers_all(mo=None,ospf={}, p_id=None):
+    try:
+        if not mo:
+            return 0
+        ospfprocess = mo.scripts.commands(commands = ['dis ospf asbr-summary | i OSPF'])
+        prlst = ospfprocess['output'][0].split('\n')
+        for pritem in prlst:
+            m = re.match(r'^\s+OSPF Process\s+(?P<id>\d+)\s+with\s+Router\s+ID\s+(?P<ip>\S+)$', pritem)
+            if m:
+                process_id = m.group('id')
+                if p_id:
+                    if p_id != process_id:
+                        continue
+                if not process_id in list(ospf.keys()):
+                    ospf[process_id] = {}
+                router_id = m.group('ip')
+                ospf[process_id][mo.bi_id] = {'id': router_id, 'noc_id': mo.id, 'peers': []}
+                Huawei_VRP_get_ospf_process_peers(mo, process_id, ospf[process_id][mo.bi_id]['peers'])
+        return 1
+    except:
+        return -1
+
+def Cisco_IOS_get_ospf_peers_all(mo=None,ospf={}, p_id=None):
+    try:
+        if not mo:
+            return -1
+        ospfprocess = mo.scripts.commands(commands = ['show ip ospf   | i Routing Process'])
+        prlst = ospfprocess['output'][0].split('\n')
+        for pritem in prlst:
+            m = re.match(r'^.+\"\S+\s+(?P<id>\d+)\"\s+with\s+ID\s+(?P<ip>\S+)$', pritem)
+            if m:
+                process_id = m.group('id')
+                if p_id:
+                    if p_id != process_id:
+                        continue
+                if not process_id in list(ospf.keys()):
+                    ospf[process_id] = {}
+                router_id = m.group('ip')
+                ospf[process_id][mo.bi_id] = {'id': router_id, 'noc_id': mo.id, 'peers': []}
+                Cisco_IOS_get_ospf_process_peers(mo, process_id, ospf[process_id][mo.bi_id]['peers']) 
+        return 1
+    except:
+        return -1       
+
+def Cisco_IOS_get_ospf_process_peers(mo=None, pid=None, peers=[]):
+    try:
+        if not mo:
+            return 0
+        if not pid:
+            return 0
+        ospfne = mo.scripts.commands(commands = ['show ip ospf {} nei '.format(pid)])
+        if not ospfne['output']:
+            return -1
+        l = ospfne['output'][0].split('\n')
+        if l[0]:
+            lst = l[1:]
+        else:
+            lst = l[2:]
+        ne = {}
+        re_area=re.compile(r".+In the area\s+(?P<area>\S+)\s+", re.MULTILINE | re.DOTALL)
+        for neitem in lst:
+            peer=neitem.split()
+            if peer:
+                peerdetail = mo.scripts.commands(commands = ['show ip ospf {} nei {} {}'.format(pid, peer[5], peer[0])])
+                area=''
+                if peerdetail['output']:
+                    marea = re_area.match(peerdetail['output'][0])
+                    if marea:
+                        area = marea.group('area')
+                peers.append(
+                  {
+                    'peer_id': peer[0],
+                    'state': peer[2],
+                    'peer_address': peer[4],
+                    'interface': peer[5],
+                    'area': area
+                  }
+                )
+        return 1
+    except:
+        return -1
+
+def get_ospf_peer_peers(mo, ospf, p_id, notfound):
+    #try:
+        if not mo.profile.name.replace('.','_')+'_get_ospf_peers_all' in globals():
+            if not mo.address in notfound:
+                notfound.append({mo.address:'no getter'})
+            return -1
+        r = globals()[mo.profile.name.replace('.','_')+'_get_ospf_peers_all'](mo,ospf,p_id)
+        if r != 1:
+            if not mo.address in notfound:
+                notfound.append({mo.address:'peers get error'})
+            return -1
+        print(len(ospf[p_id][mo.bi_id]['peers']))
+        for p in ospf[p_id][mo.bi_id]['peers']:
+            r = ManagedObject.objects.filter(address = p['peer_id'], is_managed=True)
+            if r:
+                if not r[0].bi_id in ospf[p_id]:
+                    print('Leaf peer {}'.format(r[0].name))
+                    get_ospf_peer_peers(r[0], ospf, p_id,notfound)
+            else:
+                if not p['peer_id'] in notfound:
+                    notfound.append({p['peer_id']:'MO noit found'})
+                continue
+        return 1
+    #except:
+    #    notfound.append(mo.address)
+    
+
+def handler():
+    ospf_topo={}        
+    area0routers =  ManagedObject.objects.filter(labels__contains=[' ospf_16143_area0'])
+    for router in area0routers:
+        routerprofile = router.profile.name.replace('.','_')
+        globals()[routerprofile+'_get_ospf_peers_all'](router,ospf_topo,'16143')
+    notfound = []
+    ospf1 = copy.deepcopy(ospf_topo)
+    for proc_item in ospf1.keys():
+        for router_item in ospf1[proc_item].keys():
+            for peer_item in ospf1[proc_item][router_item]['peers']:
+                router = ManagedObject.objects.filter(address = peer_item['peer_id'], is_managed=True)
+                if router:
+                    if not router[0].bi_id in ospf_topo[proc_item]:
+                        print('Core peer {}'.format(router[0].name))
+                        get_ospf_peer_peers(router[0], ospf_topo, proc_item,notfound, peer_item, ospf1[proc_item][router_item]['id'])
+                else:
+                    if not peer_item['peer_id'] in notfound:
+                        notfound.append({peer_item['peer_id']:'MO not found'})
+                    continue
+    with open('/tmp/ospf_topo.pickle', 'wb') as f:
+        pickle.dump(ospf_topo, f)
+    with open('/tmp/ospf_notfound.pickle', 'wb') as f:
+        pickle.dump(notfound, f)
+    #pprint(ospf_topo)
+
+connect()
+handler()
---
+++ commands/ospf_parse.py
@@ -0,0 +1,64 @@
+#python modules
+import re
+from pprint import pprint
+import pickle
+import json
+#NOC modules
+from noc.sa.models.managedobject import ManagedObject
+from noc.core.mongo.connection import connect
+
+class OSPF_Topo:
+    raw = {}
+    links=[]
+    js={}
+
+    def getmo(self,ip):
+        mo = ManagedObject.objects.filter(address = ip)
+        if mo:
+            return mo[0]
+        else:
+            return None
+    def makelinks(self):
+        for k,v in self.raw.items():
+            for peeritem in v['peers']:
+                link = {
+                    'area': peeritem['area'],
+                    'node1_id': v['id'],
+                    'node1_bi': k, 
+                    'node1_int': peeritem['interface'],
+                    'node2_id': peeritem['peer_id'],
+                    'node2_bi': '', 
+                    'node2_int': '',                    
+                }
+                peer_mo = self.getmo(peeritem['peer_id'])
+                if peer_mo:
+                    link['node2_bi'] = peer_mo.bi_id
+                    if peer_mo.bi_id in list(self.raw.keys()):
+                        for peer_peeritem in self.raw[peer_mo.bi_id]['peers']:
+                            if peer_peeritem['peer_id'] == v['id']:
+                                link['node2_int'] = peer_peeritem['interface']
+                self.links.append(link)
+    def makejs(self):
+        self.js={'nodes': [], 'edges': [] }
+        for k,v in self.raw.items():
+            self.js['nodes'].append({ 'id': k, 'type': 'server', 'label': v['id']})
+        for k in self.links:
+            self.js['edges'].append({
+                'source': k['node1_bi'],
+                'target': k['node2_bi'],
+                'label': k['node1_int']
+            })
+
+
+
+connect()
+topology = OSPF_Topo()
+with open('/tmp/ospf_topo.pickle', 'rb') as f:
+         data = pickle.load(f)
+topology.raw = data['16143']
+topology.makelinks()
+topology.makejs()
+s = json.dumps(topology.js)
+with open('/tmp/ospf_topo.js', 'w') as f2:
+    f2.write('const elements = ' + s+';\r\n')
+    f2.write("Diagram('scheme1', elements);\r\n")
