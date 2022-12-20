82c31de6f18e079187d93a7f4c0b5f4667777ec0
santor72 <santor@ccs.ru>
2022-12-20 12:37:31 +0300

    sfedrg

--- sa/profiles/Cisco/IOS/get_interfaces.py
+++ sa/profiles/Cisco/IOS/get_interfaces.py
@@ -424,7 +424,7 @@ class Script(BaseScript):
             ip = match.group("ip")
             ipv4_interfaces[c_iface] += [ip]
         self.logger.info('IPV4 interfaces')
-        self.logger.info(pprint(ipv4_interfaces))
+        #self.logger.info(pprint(ipv4_interfaces))
         # Get IPv6 interfaces
         ipv6_interfaces = defaultdict(list)  # interface -> [ipv6 addresses]
         c_iface = None
