c8bf97584e3f260acca96a1b22fe9c293b5ee793
santor72 <santor@ccs.ru>
2023-02-03 12:24:54 +0300

    edrt

---
+++ handlers/test.py
@@ -0,0 +1,14 @@
+ coding: utf-8
+#python modules
+import re
+from pprint import pprint
+import pickle
+import copy
+
+#NOC modules
+from noc.core.mongo.connection import connect
+from noc.sa.models.managedobject import ManagedObject
+from noc.custom.lib.ospf import *
+
+def hk_test_handler(djob):
+    return dir(djob)
