6c1ddd844fe383ff96ce0a6b392650584d17504b
santor72 <santor@ccs.ru>
2023-03-01 10:43:51 +0300

    sadf

---
+++ commands/testetl.py
@@ -0,0 +1,7 @@
+# NOC modules
+from noc.sa.models.managedobject import ManagedObject
+from noc.inv.models.networksegment import NetworkSegment
+from noc.sa.models.administrativedomain import AdministrativeDomain
+from noc.sa.models.authprofile import AuthProfile
+from noc.sa.models.managedobjectprofile import ManagedObjectProfile
+from noc.sa.models.profile import Profile
