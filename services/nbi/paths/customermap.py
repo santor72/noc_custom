adf6f5df05cd4775a3d75e7594660b2b43204c50
santor72 <santor@ccs.ru>
2023-12-28 15:09:39 +0300

    sadfasdf

--- services/nbi/paths/customermap.py
+++ services/nbi/paths/customermap.py
@@ -18,17 +18,119 @@ from noc.services.nbi.base import NBIAPI, API_ACCESS_HEADER, FORBIDDEN_MESSAGE
 from noc.sa.models.managedobject import ManagedObject
 from noc.inv.models.interface import Interface
 from noc.inv.models.interfaceprofile import InterfaceProfile
-from noc.inv.models.link import Link
+from noc.Linkin IIv.models.link import Link
 from noc.core.mongo.connection import connect
 
 router = APIRouter()
 
+class TopologyInfo:
+    nodes={}
+    links={}
+    current_link_id = 1
+    current_node_id = 1
+    node_id_map = []
+    link_id_map = []
+#Возвращает id из словаря links по хешу линка
+    def map_link_id(self, link_hash):
+        for l in links:
+            if link_hash == l.get('hash'):
+                return l.get('id')
+        return 0
+#возвращает id из словаря nodes по хешу устройства
+    def map_node_id(self, node_hash):
+        for n in nodes:
+            if node_hash == n.get('hash'):
+                return n.get('id')
+        return 0
+#Новое устройство из данных userside    
+    def newUSnode(**kwarg):
+        newid=self.current_node_id
+        self.current_node_id+=1
+        self.nodes[newid] = {
+            'id': newid,
+            'hash': self.generate_node_hash(host)
+            'system': 'userside',
+            'device_id': ID,
+            'host': host,
+            'ip': host,
+            'location': location,
+            'name': name or nazv,
+            'interfaces': ifaces,
+            'uplink_interfaces': uplink_iface_array
+        }
+        node_id_map.append({'id':newid, 'hash' = self.generate_node_hash(host)})
+        return newid
+
+    def newUSlink(a,b, inta, intb):
+        newlinkhash = self.generate_link_hash(a['ip'], b['ip'], inta['ifIndex'], intb['ifIndex'])
+        if self.map_link_id(newlinkhash):
+            return 0
+        newlinkid=self.current_link_id
+        self.current_link_id+=1
+        links[newlinkid]={
+            'id' : newlinkid,
+            'hash': newlinkhash,
+            'connect_id':connect_id,
+            'type': 'device',
+            'system': 'userside',
+            'nodea': a['id'],
+            'nodeb':b['id'],
+            'inta':{'ifindex': inta['ifIndex'], 'ifname': inta['ifName']},
+            'intb': {'ifindex': intb['ifIndex'], 'ifname': intb['ifName']}
+            }
+
+    #ищет запись о устройстве с nodes по id в учетной системе
+    def findnode_by_device_id(object_id,extsys):
+        for x in nodes.keys():            
+            if object_id == nodes[x].get('device_id') and extsys == nodes[x].get('system'):
+                return x
+        return 0
+
+    def findnode_id_byip(self, ip):
+        for x in nodes.keys():            
+            if ip == nodes[x].get('ip'):
+                return x
+        return 0
+    
+    def generate_node_hash(self,ipstr=None):
+        ip = netaddr.IPAddress(ipstr)
+        return ip.value
+
+    def generate_link_hash(self,ip1,ifnum1,ip2,ifnum2):
+        if ip1 > ip2:
+            idstr=f"{ip2}-{ifnum2}-{ip1}-{ifnum1}"
+        elif ip1 < ip2:
+            idstr=f"{ip1}-{ifnum1}-{ip2}-{ifnum2}"
+        return idstr
+    def generatejs(self):
+        topology_dict = {'nodes': [], 'links': []}
+        for k,item in self.nodes.items():
+            topology_dict['nodes'].append({
+                'id': int(item['id']),
+                'name': 'ММТС-9' if item.get('ip') in ['217.76.46.108','217.76.46.119','10.76.33.82'] else item.get('host'),
+                'primaryIP': item.get('host') or item.get('ip'),
+                'nazvanie': item.get('nazv'),
+                'location': item.get('location'),
+                'icon': 'host' if item['type'] == 'customer' else ('cloud' if item.get('ip') in ['217.76.46.108','217.76.46.119','10.76.33.82'] else 'switch')
+            })
+        for k,item in links.items():
+            topology_dict['links'].append({
+                'id': int(item['id']),
+                'source': int(item['nodea']),
+                'target': int(item['nodeb']),
+                'srcIfName': item['inta']['ifName'],
+                'tgtIfName': item['intb']['ifName'],
+                'srcDevice': int(item['nodea']),
+                'tgtDevice': int(item['nodeb'])
+            })   
+        return topology_dict
+             
 class CustomerMapfResponse(BaseModel):
     result: List[Dict]
 
 class CustomerMapRequest(BaseModel):
     customer_id: int
-
+ 
 class CustomerMapAPI(NBIAPI):
     api_name = "customermap"
     openapi_tags = ["customermap API"]
@@ -46,89 +148,64 @@ class CustomerMapAPI(NBIAPI):
         }
         return [route_post]
 
-    def findnodebyid(self, nodes,dev_id):
-        for x in nodes.keys():            
-            if dev_id == nodes[x].get('id'):
-                return x
-        return 0
+    #Ищет данные устройства в Userside по ID
+    def get_usdevice_by_id(self, dev_type, device_id):
+        result = {}
+        nodeid = topoinfo.findnode_id_byip(dev_ip)
+        if nodeid:
+            response = requests.get(f"{self.usurl}+&cat=device&action=get_data&object_type={dev_type}&object_id={device_id}")
+            if(response.ok):
+                data = json.loads(response.content)
+                if data['Result'] == 'OK':
+                    result = data['data']
+        return result
 
-    def generate_link_id(self,ip1,ifnum1,ip2,ifnum2):
-        if ip1 > ip2:
-            idstr=f"{ip2}-{ifnum2}-{ip1}-{ifnum1}"
-        elif ip1 < ip2:
-            idstr=f"{ip1}-{ifnum1}-{ip2}-{ifnum2}"
-        return idstr
+    def get_usdevice_commutation(self, devtype=None, device_id=None):
+        result = {}
+        if device_id:
+            response = requests.get(f"{self.usurl}&cat=commutation&action=get_data&object_type={devtype}&object_id={device_id}")
+            if(response.ok):
+                data = json.loads(response.content)
+                if data['Result'] == 'OK':
+                    result = data['data']
+        return result
 
-    def generate_node_id(self,ipstr='1.1.1.1'):
-        ip = netaddr.IPAddress(ipstr)
-        return ip.value
-
-    def getnode(self, dev_type, dev_id):
-        response = requests.get(f"{self.usurl}+&cat=device&action=get_data&object_type={dev_type}&object_id={dev_id}")
-        if(response.ok):
-            data = json.loads(response.content)
-            return data['data'].get(str(dev_id))
-        else:
-            return {}
+    def get_links(self, topoinfo, cur_dev_type, cur_device_ip):
+        cur_node = topoinfo.nodes.get(findnode_id_byip(cur_device_ip))
+        commutations = self.get_usdevice_commutation(topoinfo, cur_dev_type, cur_device_ip)
+        if commutations and cur_node:
+            #Перебираем соединения, если интерфейс устройства на имеет признак Uplink переходим к следующему интерфейсу
+            for cur_device_interface in commutations:
+                if not cur_device_interface in cur_node.get('uplink_interfaces'):
+                    continue
+                #Цикл по списку коммутаций интерфейса
+                for item in commutations[cur_device_interface]:
+                    if item['object_type']=='switch' or item['object_type']=='radio':
+                    #Проверяем наличие устройства с которым скоммутирован интерфейс в списке устройств
+                        newnodeid = topoinfo.findnode_by_device_id(item['object_id'],'us')
+                        if newnodeid != 0 :
+                            nextdev = topoinfo.nodes[newnodeid]
+                        #если его нет создаем
+                        else:    
+                            devdata = self.get_usdevice_by_id(item['object_type'], item['object_id'])   
+                            nextnodeid = topoinfo.newUSnode(devdata)  
+                            nextdev =  topoinfo.nodes[nextnodeid]     
+                        ifnum =  item.get('interface')
+                        #Создать линк
+                        deva = cur_node['id']
+                        devb = nextdev['id']
+                        inta = cur_node['interfaces']['cur_device_interface']
+                        intb = nextdev['interfaces'][ifnum]
+                        topoinfo.newUSlink(deva, devb, inta, intb)   
+                        if (nextdev['ip']!='217.76.46.108' and nextdev['ip']!='217.76.46.119' and nextdev['ip']!='10.76.33.82'):
+                            self.get_links(topoinfo, item['object_type'], ['ip'])
     
-    def get_links(self, dev_type, dev_id, nodes, links):
-        #Получить списко коммутаций устойства
-        response = requests.get(f"{self.usurl}&cat=commutation&action=get_data&object_type={dev_type}&object_id={nodes[dev_id]['id']}")
-        if(response.ok):
-            data = json.loads(response.content)
-            if data['Result'] == 'OK':
-                #Перебираем соединения, если интерфейс устройства на имеет признак Uplink переходим к следующему интерфейсу
-                for k in data['data']:
-                    if not k in nodes[dev_id].get('uplink_ifaces'):
-                        continue
-                    #Цикл по списку коммутаций интерфейса
-                    for item in data['data'][k]:
-                        #Проверяем наличие устройства с которым скоммутирован интерфейс в списке устройств nodes
-                        if item['object_type']=='switch' or item['object_type']=='radio':
-                            newnodeid = self.findnodebyid(nodes,item['object_id'])
-                            if newnodeid != 0 :
-                                devdata = nodes[newnodeid]
-                                ifaces = devdata.get('ifaces')
-                                uplink_ifaces = devdata.get('uplink_iface_array')
-                            else:    
-                                devdata = self.getnode(item['object_type'], item['object_id'])   
-                                newnodeid = self.generate_node_id(devdata['host'])        
-                                ifaces = devdata.get('ifaces')
-                                uplink_ifaces = devdata.get('uplink_iface_array')
-                                nodes[newnodeid] = {'id': item['object_id'],
-                                                            'type':item['object_type'],
-                                                            'nazv': devdata.get('nazv'),
-                                                            'location': devdata.get('location'),
-                                                            'uzelcode': devdata.get('uzelcode'),
-                                                            'host': devdata.get('host'),
-                                                            'ip': devdata.get('host'),
-                                                            'ifaces': ifaces,
-                                                            'uplink_ifaces': uplink_ifaces
-                                                            }
-                            ifnum =  item.get('interface')
-                            #Создать id для линка
-                            newlinkid = self.generate_link_id(dev_id,nodes[dev_id][k].get('ifIndex'),newnodeid,ifaces['ifnum']['ifIndex'])
-                            if newlinkid in links:
-                                continue
-                            links[newlinkid]={
-                                'id':item['connect_id'],
-                                'nodea': nodes[dev_id].get('ip'),
-                                'nodeb':item['host'],
-                                'inta':nodes[dev_id]['ifaces'].get(k),
-                                'intb': ifaces.get(str(ifnum))
-                                }
-                            if (devdata.get('host')!='217.76.46.108' and devdata.get('host')!='217.76.46.119' and devdata.get('host')!='10.76.33.82'):
-                                self.get_links(item['object_type'], newnodeid, nodes, links)
-    
-    async def handler(self, req:CustomerMapRequest, access_header: str = Header(..., alias=API_ACCESS_HEADER)):
+    async def handler(self, req:1CustomerMapRequest, access_header: str = Header(..., alias=API_ACCESS_HEADER)):
         result = {}
         nodes={}
         links={}
         customer={}
-        if not self.profiles:
-            iprofiles = InterfaceProfile.objects.filter(name__contains='Uplink')
-            iprofiles2 = InterfaceProfile.objects.filter(name__contains='Core')
-            self.profiles = [x for x in iprofiles] + [x for x in iprofiles2]
+        topoinfo = TopologyInfo()
         if not self.access_granted(access_header):
             raise HTTPException(403, FORBIDDEN_MESSAGE)
         connect()
@@ -142,71 +219,38 @@ class CustomerMapAPI(NBIAPI):
         else:
             result={'Result':'Fail', 'message': 'Customer find error'}
             return JSONResponse(content=result, media_type="application/json")
-        nodes[customer_id] = {}
-        nodes[customer_id] = {'id': customer_id, 
-                      'type': 'customer',
-                      'host':customer['Data']['login'],
-                      'ip': customer['Data']['ip_mac'],
-                      'location': customer['Data']['login'],
-                      'nazv': customer['Data']['login']
-                     }
+        topoinfo.nodes[topoinfo.current_node_id] = {'id': customer_id, 
+                            'type': 'customer',
+                            'host':customer['Data']['login'],
+                            'ip': customer['Data']['ip_mac'],
+                            'location': customer['Data']['login'],
+                            'nazv': customer['Data']['login']
+                            }
+        cur_node = topoinfo.nodes[topoinfo.current_node_id]
+        topoinfo.current_node_id+=1
         ac_response = requests.get(f"{self.usurl}&cat=commutation&action=get_data&object_type=customer&object_id={customer_id}")
         if(ac_response.ok):
             ac_data = json.loads(ac_response.content)
             if ac_data['Result'] == 'OK':
                 for ac_item in ac_data['data']:
                     if ac_item['object_type']=='switch' or ac_item['object_type']=='radio':
-                        devdata = self.getnode(ac_item['object_type'], ac_item['object_id'])
-                        ifnum =  ac_item.get('interface')
-                        ifaces = devdata.get('ifaces')
-                        uplink_ifaces = [x for x in devdata.get('uplink_iface_array')]
-                        ifname=''
-                        if ifnum:
-                            if ifaces.get(ifnum):
-                                ifname = ifaces[ifnum].get('ifName')
-                        links[ac_item['connect_id']]={'id':ac_item['connect_id'], 'nodea': customer_id,'inta':{'ifIndex': 1,
-                                                                                                            'ifType': 1,
-                                                                                                            'ifName': 'C',
-                                                                                                            'ifNumber': 1},
-                                                    'nodeb':ac_item['object_id'], 'intb': ifaces.get(str(ifnum))}
-                        newnodeid = self.generate_node_id(devdata.get('host'))
-                        nodes[newnodeid] = {'id': ac_item['object_id'],
-                                                    'type':ac_item['object_type'],
-                                                    'nazv': devdata.get('nazv'),
-                                                    'location': devdata.get('location'),
-                                                    'uzelcode': devdata.get('uzelcode'),
-                                                    'host': devdata.get('host'),
-                                                    'ip': devdata.get('host'),
-                                                    'ifaces': ifaces,
-                                                    'uplink_ifaces': uplink_ifaces
-                                                    }
-                        self.get_links(ac_item['object_type'], newnodeid, nodes, links)
+                        devdata = self.get_usdevice_by_id(ac_item['object_type'], ac_item['object_id'])
+                        nextnodeid = topoinfo.newUSnode(devdata)  
+                        nextdev =  topoinfo.nodes[nextnodeid]  
+                        deva = cur_node['id']
+                        devb = nextdev['id']
+                        inta = {'ifIndex': 1,'ifType': 1,'ifName': 'C','ifNumber': 1}
+                        intb = nextdev['interfaces'][ifnum]
+                        topoinfo.newUSlink(deva, devb, inta, intb)   
+                        if (nextdev['ip']!='217.76.46.108' and nextdev['ip']!='217.76.46.119' and nextdev['ip']!='10.76.33.82'):
+                            self.get_links(topoinfo, ac_item['object_type'], ['ip'])
             else:
                 result={'Result':'Fail', 'message': 'Fail find customer commutation'}
                 return JSONResponse(content=result, media_type="application/json")                                        
         else:
             result={'Result':'Fail', 'message': 'Fail request customer commutation'}
             return JSONResponse(content=result, media_type="application/json")  
-        topology_dict = {'nodes': [], 'links': []}
-        for k,item in nodes.items():
-            topology_dict['nodes'].append({
-                'id': int(item['id']),
-                'name': 'MSK-IX' if item.get('ip') in ['217.76.46.108','217.76.46.119','10.76.33.82'] else item.get('host'),
-                'primaryIP': item.get('host') or item.get('ip'),
-                'nazvanie': item.get('nazv'),
-                'location': item.get('location'),
-                'icon': 'host' if item['type'] == 'customer' else ('cloud' if item.get('ip') in ['217.76.46.108','217.76.46.119','10.76.33.82'] else 'switch')
-            })
-        for k,item in links.items():
-            topology_dict['links'].append({
-                'id': int(item['id']),
-                'source': int(item['nodea']),
-                'target': int(item['nodeb']),
-                'srcIfName': item['inta']['ifName'],
-                'tgtIfName': item['intb']['ifName'],
-                'srcDevice': int(item['nodea']),
-                'tgtDevice': int(item['nodeb'])
-            })                          
+        topology_dict = topoinfo.generatejs()
         result=topology_dict
         return JSONResponse(content=result, media_type="application/json")
 
