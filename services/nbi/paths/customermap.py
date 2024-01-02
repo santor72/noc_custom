cae0ed830bbf02e51c806e404a1b419ca4c06091
santor72 <santor@ccs.ru>
2024-01-02 15:36:25 +0300

    sdfg

--- services/nbi/paths/customermap.py
+++ services/nbi/paths/customermap.py
@@ -229,8 +229,8 @@ class CustomerMapfResponse(BaseModel):
 
 class CustomerMapRequest(BaseModel):
     customer_id: int
-    with_noc = 0
-    to_core=0
+    with_noc: int
+    to_core: int
  
 class CustomerMapAPI(NBIAPI):
     api_name = "customermap"
